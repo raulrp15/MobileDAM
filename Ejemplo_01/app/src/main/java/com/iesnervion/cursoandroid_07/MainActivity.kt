df045d747f0f9111abe594156c34ebc9fbf84921
raulrp15 <raulromerapavon@gmail.com>
2024-09-30 13:13:03 +0200

    Nuevo

--- Ejemplo_01/app/src/main/java/com/iesnervion/cursoandroid_07/MainActivity.kt
+++ Ejemplo_01/app/src/main/java/com/iesnervion/cursoandroid_07/MainActivity.kt
@@ -1,16 +1,30 @@
 package com.iesnervion.cursoandroid_07
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.EditText
+import androidx.constraintlayout.widget.ConstraintSet.Constraint
+
+@SuppressLint("StaticFieldLeak")
+lateinit var name:EditText
+@SuppressLint("StaticFieldLeak")
+lateinit var button:Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d(":::Vida", "He creado el create")
-        val uName:String =
+        name = findViewById<EditText>(R.id.user)
+
+        }
+
+    fun cambia(v:View){
+        setContentView(R.layout.bienvenida)
     }
 
     override fun onStart() {
@@ -25,11 +39,13 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
+        setContentView(R.layout.activity_main)
         Log.d(":::Vida", "He creado el resume")
     }
 
     override fun onPause() {
         super.onPause()
+        setContentView(R.layout.pausa)
         Log.d(":::Vida", "He creado el pause")
     }
 
